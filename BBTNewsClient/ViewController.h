bdd889d5b3299313c8eaad5c9d84ebf9268e6c76
Bill Bai <billbai42@gmail.com>
2014-11-18 23:33:33 +0800

    Initial Commit

---
+++ BBTNewsClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BBTNewsClient
+//
+//  Created by Bill Bai on 11/18/14.
+//  Copyright (c) 2014 BBT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BBTNewsClient/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BBTNewsClient
+//
+//  Created by Bill Bai on 11/18/14.
+//  Copyright (c) 2014 BBT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
